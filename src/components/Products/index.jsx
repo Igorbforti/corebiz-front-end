abfe4386e65a271aa19fb03c7b2ce5df5817b7c2
Igorbforti <igorbonuzzi@gmail.com>
2022-04-27 15:14:03 -0300

    products finished

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react'
 import Slider from 'react-slick'
+import formatValue from '../../formatValue'
 
 import './style.css'
 
-const Products = () => {
+const Products = ({addToCart}) => {
 
     const settings = {
         infinite: true,
@@ -40,7 +41,7 @@ const Products = () => {
         <Slider {...settings}>
             {products.map(({imageUrl, listPrice, price, productId, productName, stars, installments, index}, product) => {
             return (
-                <div className='card' id={productId}>
+                <div className='card' id={productId} data-product-name={productName}>
                     <div className='card-image'>
                         <img src={imageUrl} alt='' key={index} className='product-image'/>
                         {listPrice ? (<p className='discount-flag'></p>) : ''}
@@ -55,14 +56,14 @@ const Products = () => {
                             {stars > 4 ? (<span className='star-checked'></span>) : (<span className='star'></span>)}
                         </div>
                         <div className='product-price'>
-                            {listPrice ? (<p className='discount'></p>) : ''}
-                            <p className='product-price'>{price}</p>
+                            <p className='list-price'>{listPrice ? 'de' + formatValue(String(listPrice)) : ''}</p>
+                            <p className='price'>por {formatValue(String(price))}</p>
                         </div>
-                            {installments[0] ? `ou em ${installments[0].quantity} x de ${installments[0].value}` : ''} 
-                        <div>
-                            <button className='buy-button'>COMPRAR</button>
-                        </div>
+                            <p className='product-installment'>
+                            {installments[0] ? `ou em ${installments[0].quantity} x de ${formatValue(String(installments[0].value))}` : ''} 
+                            </p>
                     </div>
+                    <button className='buy-button' onClick={addToCart}>COMPRAR</button>
                 </div>
                 )
             })}
